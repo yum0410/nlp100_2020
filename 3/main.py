926afed4e41131f1edb7139235bbcabfdaab2364
FT-yuki <yakinikug1010@gmail.com>
2020-07-01 22:04:28 +0900

    solve 23 24

--- 3/main.py
+++ 3/main.py
@@ -18,5 +18,20 @@ import re
 pattern = "^\[\[Category:(.*?)(|\|.*)\]\]$"
 for line in data20["text"].split("\n"):
     if re.search(pattern, line):
-        print(re.search(pattern, line).group(1))
+        # print(re.search(pattern, line).group(1))
+        pass
 
+# 23
+pattern = "^(=+)\s*(.*?)\s*(=+)"
+for line in data20["text"].split("\n"):
+    section_line = re.search(pattern, line) 
+    if section_line is not None:
+        # print(section_line.group(2), len(section_line.group(1))-1)
+        pass
+
+# 24
+pattern = "(ファイル):(.*?)\|"
+for line in data20["text"].split("\n"):
+    section_line = re.search(pattern, line) 
+    if section_line is not None:
+        print(section_line.group(2))
